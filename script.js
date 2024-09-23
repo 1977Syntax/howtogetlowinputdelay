511e7480b662c21a702683fa7788486e1af46f63
kompiledcode <jayrealthug29@gmail.com>
2024-09-23 11:28:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // Smooth scroll effect for internal links
-document.querySelectorAll('a[href^="#"]').forEach
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
