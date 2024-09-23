9405959dd37908b3c8b709bff2068e754db87fa8
kompiledcode <jayrealthug29@gmail.com>
2024-09-23 18:22:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// Sticky Navbar Functionality
-window.onscroll = function() {
-    const navbar = document.querySelector('.navbar');
-    const sticky = navbar.offsetTop;
-
-    if (window.pageYOffset > sticky) {
-        navbar.classList.add('sticky');
-    } else {
-        navbar.classList.remove('sticky');
-    }
-};
-
-// Smooth Scrolling for Navigation Links
+// Smooth scroll for navigation
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
